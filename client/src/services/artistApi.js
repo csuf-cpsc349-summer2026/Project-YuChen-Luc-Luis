976bb89627225d00e97b6057eecea7626f705d55
ychung333 <roisu3sei.us@gmail.com>
2026-07-26 17:54:04 -0700

    update api to link with actual url

--- client/src/services/artistApi.js
+++ client/src/services/artistApi.js
@@ -2,14 +2,17 @@ export async function getArtist(artistId) {
     console.log("Artist API ID:", artistId);
 
     const response = await fetch(
-        `http://localhost:3000/api/artist/${encodeURIComponent(artistId)}`
+        `${import.meta.env.VITE_API_URL}/api/artist/${encodeURIComponent(
+            artistId
+        )}`
     );
 
     const data = await response.json();
 
     if (!response.ok) {
         throw new Error(
-            data.error || "Unable to load artist information."
+            data.error ||
+                "Unable to load artist information."
         );
     }
 
